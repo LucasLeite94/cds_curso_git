4bfd0191608877b42001a8e7c34f6b49a6dc7603
Lucas Leite <luccasp1920@hotmail.com>
2025-01-31 18:13:01 -0300

    adicionado resposta para a decima terceira pergunta da primeira rodada de perguntas

--- src/answers.py
+++ src/answers.py
@@ -2,7 +2,8 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-def rd1_question(df):
+def rd1_question_9(df):
+
     df_grouped = df[['id', 'seller_type']].groupby('seller_type')
 
     df_grouped = df_grouped.count().reset_index()
@@ -22,3 +23,28 @@ def rd1_question(df):
     st.plotly_chart(fig, use_container_width=True)
 
     return None
+
+
+
+def rd1_question_13(df):
+    df_grouped = (df.groupby('owner')
+    .agg(qty = pd.NamedAgg('id', 'count'))
+    .sort_values('qty')
+    .reset_index()
+)
+    
+    fig = px.bar(
+        df_grouped,
+        x = 'owner',
+        y = 'qty',
+        labels = {'owner': 'owner types', 'qty': 'quantity'},
+        color = 'owner',
+        text = 'qty',
+    )
+    fig.update_traces(textposition = 'outside')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    return None
+
+
